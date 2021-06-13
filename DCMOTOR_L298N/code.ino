05569f95373db39fb46d9310c349b51e6fe8611a
nhinojosa <nhinojosa@southtexascollege.edu>
2021-06-12 22:08:16 -0500

    Update code.ino

--- DCMOTOR_L298N/code.ino
+++ DCMOTOR_L298N/code.ino
@@ -1,1 +1,20 @@
 //Code Goes here
+// C++ code
+//
+
+//const int ENABLE = 5;
+const int INPUT1 = 2;
+const int INPUT2 = 3;
+void setup()
+{
+  //pinMode(ENABLE,OUTPUT);
+  pinMode(INPUT1,OUTPUT);
+  pinMode(INPUT2,OUTPUT);
+}
+
+void loop()
+{
+  	digitalWrite(ENABLE,HIGH);
+	  digitalWrite(INPUT1, LOW);
+  	digitalWrite(INPUT2, HIGH);
+}
