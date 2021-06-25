94834025035a554cba0ad711182e56b4f1c406f5
nhinojosa <nhinojosa@southtexascollege.edu>
2021-06-25 09:21:38 -0500

    Update code.ino

--- SD/code.ino
+++ SD/code.ino
@@ -2,11 +2,14 @@
 
 
 const int LEFT_TRIG = 13;
-const int LEFT_ECHO = 10;
-const int RIGHT_TRIG = 11;
-const int RIGHT_ECHO = 8;
 const int FRONT_TRIG = 12;
+const int RIGHT_TRIG = 11;
+
+const int LEFT_ECHO = 10;
+const int RIGHT_ECHO = 8;
 const int FRONT_ECHO = 9;
+
+
 LiquidCrystal_PCF8574 lcd(0x27); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
@@ -26,7 +29,7 @@ void setup()
 void loop()
 {
   
-  double left_distance, right_distance, front_distance;
+  double left_distance = 100, right_distance = 100, front_distance = 100;
   left_distance = ping(LEFT_TRIG, LEFT_ECHO);
   right_distance = ping(RIGHT_TRIG, RIGHT_ECHO);
   front_distance = ping(FRONT_TRIG, FRONT_ECHO);
